9f5275360deadf673cfd778a9a268de7b1cb236e
André Almeida <aluno19421@ipt.pt>
2019-04-12 00:51:21 +0100

    Introdução e utilização simples de string

--- PeddyPilot/App.js
+++ PeddyPilot/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput, Button} from 'react-native';
 
 export default class App extends React.Component {
   state={
-    text: "Hello World"
+    name: "" , text: "Introduza um nome " , newMess: ""
   }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.text}</Text>
+      <Text>{this.state.text}</Text>
+        <TextInput
+        style={{marginBottom:10, height: 40, width: 150, borderColor: 'gray', borderWidth: 1, textAlign: "center"}}
+        selectTextOnFocus={true}
+        autoFocus={true}
+        onChangeText={(name) => this.setState({name})}
+      />
+      <Button title="Submeter nome" onPress={this.onAfterName}></Button>
+      <Text style={{marginTop:10}}>{this.state.newMess}</Text>
       </View>
     );
+  }
+
+  onAfterName = () =>{
+    this.setState({newMess: "Olá " + this.state.name})
   }
 }
 
