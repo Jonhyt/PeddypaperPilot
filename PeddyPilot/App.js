bc4b2d5bc3ed781ce79096e9ead9c014482f268b
André Almeida <aluno19421@ipt.pt>
2019-05-14 16:57:42 +0100

    Home screen and QR_reader screen

--- PeddyPilot/App.js
+++ PeddyPilot/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
-// import Navigation from 'react-navigation';
+import { View, Text, Button } from 'react-native';
+import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; 
 
 import CameraPage from './src/camera.page';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <CameraPage />
-        );
-    };
-};
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to QR Reader"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'QR_reader' })
+              ],
+            }))
+          }}
+        />
+      </View>
+    );
+  }  
+}
+
+class QR_readerScreen extends React.Component {
+  render() {
+    return (
+        <CameraPage/>
+    );
+  }  
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  QR_reader: {
+    screen: QR_readerScreen,
+  },
+}, {
+    initialRouteName: 'Home',
+});
+
+export default createAppContainer(AppNavigator);
